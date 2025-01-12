269841158d0fdc700838598bbc18a0418387844d
Zack <168221270+Zval321@users.noreply.github.com>
2025-01-12 16:15:28 -0600

    added functionality to send the quantity of items to menu

--- src/Menu/Menu.jsx
+++ src/Menu/Menu.jsx
@@ -10,6 +10,7 @@ export function Menu() {
   const [category, setCategory] = useState([]);
   const [isFoodVisible, setIsFoodVisible] = useState(false);
   const [currentFood, setCurrentFood] = useState({});
+  const [quantity, setQuantity] = useState(1)
   const apiKey = import.meta.env.VITE_API_KEY;
 
   const handleIndex = async () => {
@@ -39,15 +40,15 @@ export function Menu() {
     console.log('handleClose');
     setIsFoodVisible(false);
   };
-  
-  const handleAddToCart = async (event, FoodId) => { 
+
+  const handleAddToCart = async (event, FoodId, quantity) => { 
     event.preventDefault();
     console.log('ordered item');
     try {
       const response = await axios.post(`${apiKey}/cart_items.json`, { 
         food_id: FoodId, 
         cart_id: 1,
-        quantity: 1
+        quantity: quantity
       })
       if(response.status === 201) {  
         console.log('successfully added to cart', response.data)
@@ -61,43 +62,57 @@ export function Menu() {
 
   return (
     <div>
-      <div>
-        <Header />
-      </div>
-      <div>
-        <a href="/Menu/Menu.pdf" target="_blank">Menu</a>
-      </div>
-      <div>
-        <h1>Order for Delivery Below</h1>
-      </div>
-      <div className='container-sushi-menu'>
-        {Array.isArray(category) && category.length > 0 ? (
-          category.map((categoryItem) => (
-            <div key={categoryItem.id} className="category-item">
-              <h2 className="category-name">{categoryItem.name}</h2>
-              <div className="food-items">
-                {Array.isArray(categoryItem.foods) && categoryItem.foods.length > 0 ? (
-                  categoryItem.foods.map((foodItem, foodIndex) => (
-                    <div key={foodIndex} className="food-item">
-                      <h3>{foodItem.name}</h3>
-                      <h4>Price: ${foodItem.price}</h4>
-                      <img className="image" src={foodItem.image_url} alt={foodItem.name} />
-                      <div className="button-container">
-                        <button className="order-button" onClick={ (event) => handleAddToCart(event, foodItem.id)}>Add to Cart</button>
-                        <button className="more-info-button" onClick={() => handleShow(foodItem)}>More info</button>
+    <Header />
+    <div>
+      <a href="/Menu/Menu.pdf" target="_blank">Menu</a>
+    </div>
+    <div>
+      <h1>Order for Delivery Below</h1>
+    </div>
+    <div className='container-sushi-menu'>
+      {Array.isArray(category) && category.length > 0 ? (
+        category.map((categoryItem) => (
+          <div key={categoryItem.id} className="category-item">
+            <h2 className="category-name">{categoryItem.name}</h2>
+            <div className="food-items">
+              {Array.isArray(categoryItem.foods) && categoryItem.foods.length > 0 ? (
+                categoryItem.foods.map((foodItem, foodIndex) => (
+                  <div key={foodIndex} className="food-item">
+                    <h3>{foodItem.name}</h3>
+                    <h4>Price: ${foodItem.price}</h4>
+                    <img className="image" src={foodItem.image_url} alt={foodItem.name} />
+                    <form onSubmit={(event) => handleAddToCart(event, foodItem.id, foodItem.quantity)}>
+                      <div className="form-group">
+                        Quantity: 
+                        <input
+                          type='number'
+                          min='1'
+                          defaultValue={1}
+                          onChange={(event) => foodItem.quantity = event.target.value} // updates quantity on change
+                        />
                       </div>
-                    </div>
-                  ))
-                ) : (
-                  <p>No food items available</p>
-                )}
-              </div>
+                      <div className="button-container">
+                        <button type="submit" className="order-button">Add to Cart</button>
+                        <button 
+                          className="more-info-button" 
+                          onClick={() => handleShow(foodItem)}
+                        >
+                          More info
+                        </button>
+                      </div>
+                    </form>
+                  </div>
+                ))
+              ) : (
+                <p>No food items available</p>
+              )}
             </div>
-          ))
-        ) : (
-          <p>Loading categories...</p>
-        )}
-      </div>
+          </div>
+        ))
+      ) : (
+        <p>Loading categories...</p>
+      )}
+    </div>
 
       <Modal show={isFoodVisible} onClose={handleClose}>
         {currentFood && (
