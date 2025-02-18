7d85524be3626374b94344f5e563dc405169eb6d
Zack <168221270+Zval321@users.noreply.github.com>
2025-02-18 16:10:50 -0600

    added index action to the orders page

--- src/Orders/Orders.tsx
+++ src/Orders/Orders.tsx
@@ -1,6 +1,24 @@
 import react from 'react'
+import axios from 'axios'
+import { useEffect } from 'react'
 
 export function Orders() {
+  const apiKey = import.meta.env.VITE_API_KEY;
+
+  const handleIndex = async () => {
+    try {
+      const response = await axios.get(`${apiKey}/orders.json`)
+      console.log(response.data)
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+  useEffect(() => {
+    handleIndex();
+  }, [])
+
+
   return (
     <h1>Hello</h1>
   )
